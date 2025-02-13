4abbef4788ecabbfeaa129076afa8e825c730cc5
thales <thales.savkli@gmail.com>
2025-02-13 13:10:07 -0500

    saving prog

---
+++ data/sample_code_bad.py
@@ -0,0 +1,19 @@
+# woof wooif bark bark
+
+def add(a, b):
+    """m,mnxcv,nmxvcm,n."""
+    # sdfsdfsdfsdf
+    return a + b
+
+class Calculator:
+    """
+    34653efgdfg
+    """
+    def multiply(self, a, b):
+        # uiotyuip
+        result = a * b  # cvxxcvb
+        return result
+
+def undocumented_function(x):
+    # 6749476478
+    return x * 2
